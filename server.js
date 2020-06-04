96883ab1a900a32433f8d8442a5d72a0c68affca
Glitch (hello-express) <none>
2020-06-04 08:55:23 +0000

    🚋⛷ Checkpoint ./package.json:5855833/68 ./server.js:5855833/226

--- server.js
+++ server.js
@@ -6,6 +6,15 @@
 const express = require("express");
 const app = express();
 
+const dialogflow = require('dialogflow');
+const uuid = require('uuid');
+
+const bodyParser = require('body-parser')
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 // our default array of dreams
 const dreams = [
   "Find and count some sheep",
