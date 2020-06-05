b7484af8a4dbf1c04f6e8c3d1697e38a18b4b116
Glitch (hello-express) <none>
2020-06-05 08:22:21 +0000

    🌌🐫 Checkpoint ./server.js:5855833/9 ./public/chat.css:5855833/774

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/views/chat.html");
 });
 
 app.get("/chat", (request, response) => {
